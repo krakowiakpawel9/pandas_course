cdebc92fb9cdcb0e470f49f0e43dc8d251f00de6
Paweł Krakowiak <37594623+krakowiakpawel9@users.noreply.github.com>
2019-05-13 11:55:09 +0200

    Create 02_append.py

---
+++ struktury_danych/06_merge_join_concatenate/02_append.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+import numpy as np
+import pandas as pd
+
+
+df1 = pd.DataFrame(np.random.rand(10, 4), columns=list('abcd'))
+df2 = pd.DataFrame(np.random.rand(10, 4), columns=list('abcd'))
+df3 = pd.DataFrame(np.random.rand(10, 4), columns=list('abcd'))
+
+# %%  
+df = df1.append(df2).sort_index().reset_index()
+del df['index']
+
+# %%
+df = df1.append(df2).sort_index().reset_index().drop('index', axis=1)
+
+# %%
+df = df1.append(df2, ignore_index=True)
+
+
+
