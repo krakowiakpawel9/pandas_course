a472a47cc53fe24800591e4292ea4a2cf723ff41
Paweł Krakowiak <37594623+krakowiakpawel9@users.noreply.github.com>
2019-05-13 13:40:50 +0200

    Create 01-series.py

---
+++ struktury_danych/text_data/01-series.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-
+import numpy as np
+import pandas as pd
+
+
+s = pd.Series(['Apple', '   Microsoft', np.nan, '  Google  ', 'Anazon'])
+
+# %%
+s = s.str.strip()
+
+# %%
+lower = s.str.lower()
+
+# %%
+upper = s.str.upper()
+
+# %%
+length = s.str.len()
+
+# %% case
+df = pd.DataFrame(np.random.rand(10, 2),
+                  columns=['          ID value ', '  Price'])
+
+df.columns = df.columns.str.strip()
+
+# %%
+df.columns = df.columns.str.lower()
+
+# %%
+df.columns = df.columns.str.replace(' ', '_')
+
