30aa852cca414d547ccafc7d7885dc42a08e076a
Paweł Krakowiak <37594623+krakowiakpawel9@users.noreply.github.com>
2019-05-13 09:22:39 +0200

    Create 08_index.py

---
+++ struktury_danych/05_slicing/08_index.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+
+import pandas as pd
+import numpy as np
+
+# %%
+np.random.seed(0)
+idx = pd.Index(['5364', '3445', '3343', '5732'])
+
+df = pd.DataFrame(np.random.randn(4, 5),
+                  columns=list('abcde'),
+                  index=idx)
