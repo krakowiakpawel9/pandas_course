b5a1054188307fa650c0bd487d33c0cc53b23532
Paweł Krakowiak <37594623+krakowiakpawel9@users.noreply.github.com>
2019-05-13 09:20:37 +0200

    Create 02_loc_date_range.py

---
+++ struktury_danych/05_slicing/02_loc_date_range.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+
+
+import pandas as pd
+import numpy as np
+
+# %%
+np.random.seed(0)
+df = pd.DataFrame(np.random.randn(31, 5),
+                  columns=list('abcde'),
+                  index=pd.date_range('20190101', periods=31))
+
+idx = df.index
+
+day = df.loc['2019-01-01']
+day_df = df.loc[:'2019-01-01']
+
+week = df.loc['2019-01-01':'2019-01-07']
+after_15 = df.loc['2019-01-15':]
+before_15 = df.loc[:'2019-01-15']
