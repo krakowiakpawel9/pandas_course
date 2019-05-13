18a45f991e71483bad5b1956af91b2b0c623a00d
Paweł Krakowiak <37594623+krakowiakpawel9@users.noreply.github.com>
2019-05-13 09:21:20 +0200

    Create 04_dot.py

---
+++ struktury_danych/05_slicing/04_dot.py
@@ -0,0 +1,36 @@
+# -*- coding: utf-8 -*-
+
+import pandas as pd
+import numpy as np
+
+
+# %%
+np.random.seed(0)
+df = pd.DataFrame(np.random.randn(20, 5),
+                  columns=list('abcde'),
+                  index=list('abcdefghijklmnoprstu'))
+
+# %%
+
+col_a = df.a
+col_b = df.b
+
+# %%
+mask = df.a > 0
+df[mask]
+
+# %%
+mask = (df.a > 0) & (df.c > 0)
+df[mask]
+
+# %%
+mask = df.a > 1
+df[mask]
+
+# %% 
+mask = (df.a > 1) | (df.c > 1)
+df[mask]
+df[mask][['a', 'c']]
+
+# %% change column
+df.e = np.random.randint(0, 10, 20)
