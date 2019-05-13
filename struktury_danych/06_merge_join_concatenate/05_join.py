3d71da5b53fa49888e30a6560eca23c5f229bb5f
Paweł Krakowiak <37594623+krakowiakpawel9@users.noreply.github.com>
2019-05-13 11:56:15 +0200

    Create 05_join.py

---
+++ struktury_danych/06_merge_join_concatenate/05_join.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+import numpy as np
+import pandas as pd
+
+
+left = pd.DataFrame(np.random.rand(10, 3),
+                   index=pd.date_range('2019-01-01', periods=10),
+                   columns=list('abc'))
+
+right = pd.DataFrame(np.random.rand(10, 3),
+                   index=pd.date_range('2019-01-04', periods=10),
+                   columns=list('def'))
+
+# %% left
+left_join = left.join(right, how='left')
+
+# %% right
+right_join = left.join(right, how='right')
+
+# %% inner
+inner_join = left.join(right, how='inner')
+# %% outer
+outer_join = left.join(right, how='outer')
+
