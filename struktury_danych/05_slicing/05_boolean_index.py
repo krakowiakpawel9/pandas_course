f9ae78fa3496d36b98c4887a85704d5a527490f3
Paweł Krakowiak <37594623+krakowiakpawel9@users.noreply.github.com>
2019-05-13 09:21:40 +0200

    Create 05_boolean_index.py

---
+++ struktury_danych/05_slicing/05_boolean_index.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+
+import pandas as pd
+import numpy as np
+
+# %%
+np.random.seed(0)
+df = pd.DataFrame(np.random.randn(31, 5),
+                  columns=list('abcde'),
+                  index=pd.date_range('20190101', periods=31))
+
+# %%
+mask = df > 0
+df_gt_0 = df[mask]
+
+df_gt_0 = df[df > 0]
+
+# %%
+df[df < -1]
+df[(df < -1) | (df > 1)]
+df[(df > 0.5) & (df < 1.)]
+
+df[df < 0]
+df[~(df < 0)]
+
+df[(df < 0) & ~(df < 0)]
+df[(df < 0) | ~(df < 0)]
+
+# %% 
