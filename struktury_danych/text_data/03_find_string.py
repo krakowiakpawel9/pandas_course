1c886d8c0b9a41c9ee3c8e53212ce71028d008ec
Paweł Krakowiak <37594623+krakowiakpawel9@users.noreply.github.com>
2019-05-13 13:41:38 +0200

    Create 03_find_string.py

---
+++ struktury_danych/text_data/03_find_string.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+import numpy as np
+import pandas as pd
+
+
+string = 'workout summer good free holiday time time hot'
+split_string = string.split(' ')
+
+s = pd.Series(split_string)
+
+# %
+s.str.contains(r'[0-9]')
+s.str.contains(r'[a-z]')
+
+s.str.contains(r'[rg]')
