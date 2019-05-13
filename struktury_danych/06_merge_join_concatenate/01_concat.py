0c91c95b5dd92bcbe1e5fcacc4f5ae03aed7958d
Paweł Krakowiak <37594623+krakowiakpawel9@users.noreply.github.com>
2019-05-13 11:54:49 +0200

    Create 01_concat.py

---
+++ struktury_danych/06_merge_join_concatenate/01_concat.py
@@ -0,0 +1,39 @@
+# -*- coding: utf-8 -*-
+import numpy as np
+import pandas as pd
+
+
+df1 = pd.DataFrame(np.random.rand(10, 4), columns=list('abcd'))
+df2 = pd.DataFrame(np.random.rand(10, 4), columns=list('abcd'))
+df3 = pd.DataFrame(np.random.rand(10, 4), columns=list('abcd'))
+s = pd.Series(np.random.rand(10), name='x')
+
+# %% concat
+df = pd.concat([df1, df2, df3], ignore_index=True)
+
+# %% 
+df = pd.concat([df1, df2, df3])
+df.reset_index()
+
+# %%
+df1 = pd.DataFrame(np.random.rand(10, 4), columns=list('abcd'))
+df2 = pd.DataFrame(np.random.rand(10, 4), columns=list('efgh'))
+
+df = pd.concat([df1, df2])
+df = pd.concat([df1, df2], axis=1)
+
+# %%
+
+df1 = df1[::2]
+df = pd.concat([df1,  df2], axis=1, join='outer')
+df = pd.concat([df1, df2], axis=1, join='inner')
+
+# %%
+# append Series to DataFrame
+pd.concat([df1, s])
+pd.concat([df1, s], axis=1)
+
+# %%
+df1.columns
+df2.columns = ['e', 'f', 'g', 'h']
+df = pd.concat([df1, df2], axis=1)
