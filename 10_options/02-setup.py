cc204e6e096f7f1e1cc20497169d89ff040b3a91
Paweł Krakowiak <37594623+krakowiakpawel9@users.noreply.github.com>
2019-05-14 11:36:41 +0200

    Create 02-setup.py

---
+++ 10_options/02-setup.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+pd.set_option('display.max_rows', 999)
+pd.set_option('precision', 3)
+
+pd.describe_option('precision')
+pd.get_option('expand_frame_repr')
+pd.set_option('large_repr', 'info')
+# %%
+df = pd.DataFrame(np.random.rand(100, 4),
+                  columns=['a', 'b', 'cust_tab_dev_prod_dict_flg', 'd'])
+print(df)
+# %%
+df.info()
+df.memory_usage()
