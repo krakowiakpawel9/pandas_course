f720b4d9dd7ecc20cd6582366bd1fe7ddd563cec
Paweł Krakowiak <37594623+krakowiakpawel9@users.noreply.github.com>
2019-05-14 11:35:25 +0200

    Create 06_binary_window.py

---
+++ 09_computational/06_binary_window.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+df = pd.read_csv('./data/ten_d.csv', index_col=0)
+df.columns = ['Open', 'High', 'Low', 'Close', 'Volume']
+
+# %%
+open_price = df['Open']
+close_price = df['Close']
+df['Daily_Change'] = df['Close'].pct_change()
+corr = df.corr()
+
+# %%
+rolling_corr = open_price.rolling(window=60).corr(close_price)
+rolling_corr.plot()
+
+# %%
+df_roll = df.rolling(window=20).corr(df['Close']).drop('Close', axis=1)
+df_roll.plot()
+
+
