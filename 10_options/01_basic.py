b37cb36da9ec2d1993a27985a2fa60707f287e28
Paweł Krakowiak <37594623+krakowiakpawel9@users.noreply.github.com>
2019-05-14 11:36:18 +0200

    Create 01_basic.py

---
+++ 10_options/01_basic.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+
+# setting max rows to display
+pd.options.display.max_rows = 10
+
+# %%
+df = pd.DataFrame(np.random.randn(100, 3))
+
+# %% get value of option
+pd.get_option('display.max_rows')
+
+# %% set value of option
+pd.set_option('display.max_rows', 30)
+pd.get_option('display.max_rows')
+
+pd.reset_option('display.max_rows')
+pd.get_option('display.max_rows')
+
+pd.describe_option('display.max_rows')
+pd.describe_option('mode.sim_interactive')
+
