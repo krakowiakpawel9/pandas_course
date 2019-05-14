0bee56750e8ef7e936bd7c5dda14cac2910aa50e
Paweł Krakowiak <37594623+krakowiakpawel9@users.noreply.github.com>
2019-05-14 11:35:06 +0200

    Create 05_time_rolling.py

---
+++ 09_computational/05_time_rolling.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+
+dft =pd.DataFrame({'price': np.random.randn(97)},
+                   index=pd.date_range('20190101 09:00:00', periods=97,
+                                       freq='5min'))
+
+fake_price = dft.cumsum()
+fake_price_mean = fake_price.rolling(10).mean()
+# %%
+fake_price.plot()
+
+# %%
+stats = pd.concat([fake_price, fake_price_mean], axis=1)
+
+# %%
+fake_price_mean.plot()
+
+# %%
+fake_price
+fake_price.rolling(window=10, min_periods=5).agg(np.mean)
