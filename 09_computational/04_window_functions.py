7285b363c511af4b44e28cf88f4923e9b5d319b9
Paweł Krakowiak <37594623+krakowiakpawel9@users.noreply.github.com>
2019-05-14 11:34:45 +0200

    Create 04_window_functions.py

---
+++ 09_computational/04_window_functions.py
@@ -0,0 +1,74 @@
+# -*- coding: utf-8 -*-
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+
+"""
+count() - number of non-null observations
+sum()
+mean()
+median()
+min()
+max()
+std()
+var() - unbiased variance
+skew() - skewness - 3 moment
+kurt() - kurtosis - 4 moment
+quantile()
+apply()
+cov()
+corr()
+"""
+
+df = pd.read_csv('./data/ten_d.csv', index_col=0)
+df.columns = ['Open', 'High', 'Low', 'Close', 'Volume']
+clean_price = df[['Open', 'High', 'Low', 'Close']]
+
+# %% Series
+vol = df['Volume']
+cum_vol = vol.cumsum()
+
+# %%
+cum_vol.plot()
+
+# %%
+close = df['Close']
+close_rol = close.rolling(window=30).mean()
+close.plot()
+close_rol.plot(style='k--')
+
+# %% rolling means
+close.plot(color='black')
+for i in [5, 10, 15, 20, 60]:
+    close_rol = close.rolling(window=i).mean()
+    close_rol.plot()
+    
+# %% simple strategy
+close.plot(color='black')
+for i in [5]:
+    close_rol_min = close.rolling(window=i).min()
+    close_rol_min.plot()
+    close_rol_max = close.rolling(window=i).max()
+    close_rol_max.plot()
+    
+# %% DataFrame
+clean_price.rolling(window=20).mean().plot()
+close.plot()
+
+# %% DataFrame, std
+close.rolling(window=15).std().plot()
+close.plot()
+
+# %% DataFreame subplots
+clean_price.rolling(window=20).mean().plot(subplots=True)
+
+# %% apply, custom indicator
+aa = close.rolling(window=10).apply(lambda x: abs(x - x.mean()).mean())
+aa.plot()
+
+
+
+
+
