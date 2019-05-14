683a24e53a79d5df85c0ccb92d67edb70d087536
Paweł Krakowiak <37594623+krakowiakpawel9@users.noreply.github.com>
2019-05-14 11:33:54 +0200

    Create 02_correlation.py

---
+++ 09_computational/02_correlation.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+
+df = pd.read_csv('./data/ten_d.csv', index_col=0)
+df.columns = ['Open', 'High', 'Low', 'Close', 'Volume']
+clean_price = df[['Open', 'High', 'Low', 'Close']]
+
+# %%
+corr_matrix = clean_price.corr().round(2)
+
+# %% corr beteen Series
+df['Open'].corr(df['Close'])
+
+# %% plot correlation matrix
+plt.matshow(clean_price.corr())
+
+# %% using seaborn
+sns.heatmap(corr_matrix,
+            xticklabels=corr_matrix.columns.values,
+            yticklabels=corr_matrix.columns.values)
+
