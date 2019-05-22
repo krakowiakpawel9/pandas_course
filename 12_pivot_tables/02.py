baa09a75d2ad53578bc54e40a3fdada66e562fb6
Paweł Krakowiak <37594623+krakowiakpawel9@users.noreply.github.com>
2019-05-22 14:58:13 +0200

    Create 02.py

---
+++ 12_pivot_tables/02.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+import pandas as pd
+import numpy as np
+import seaborn as sns
+
+
+url = ('https://archive.ics.uci.edu/ml/machine-learning-databases/00492/'
+       'Metro_Interstate_Traffic_Volume.csv.gz')
+
+# %%
+metro = pd.read_csv(url, compression='gzip', parse_dates=True, index_col='date_time')
+metro = metro.loc['2016-01-01':]
+
+# %%
+traffic = metro.iloc[:, -1:]
+traffic.plot()
+tr = traffic.resample('M').sum()
+tr.plot()
+
+# %%
+metro.pivot_table(values='traffic_volume', index='weather_main').plot(kind='bar')
+
+metro.groupby('holiday').mean()['traffic_volume'].plot(kind='bar')
